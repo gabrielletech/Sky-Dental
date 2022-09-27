8cc19f785f0b9566efd12f431da4f065bc730f58
Fadziso Gabrielle <fadziso@novodigital.biz>
2022-09-27 19:11:57 +0200

    Modified & Styled Nav

---
+++ main.js
@@ -0,0 +1,8 @@
+// Toggle contact form
+function toggleContact() {
+    const toggleContact = document.querySelector(".toggleContact");
+    const contactMenu = document.querySelector(".contactMenu");
+    toggleContact.classList.toggle("c.active");
+    contactMenu.classList.toggle("c-active")
+}
+// End Toggle contact form
